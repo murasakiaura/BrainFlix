638e91e85ef6a42e584845b7768499cdc5b455eb
murasakiaura <isabella002211@gmail.com>
2024-07-07 23:53:46 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Header from "./components/Header/Header.js";
+import MainVideo from "./components/MainVideo/MainVideo.js";
+import VideoDetails from "./components/VideoDetails/VideoDetails.js";
+import Comment from "./components/Comment/Comment.js";
+import NextVideo from "./components/NextVideo/NextVideo.js";
+import videoData from "./data/videos.json";
+import videoDetailsData from "./data/video-details.json";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [currentVideo, setCurrentVideo] = useState(videoDetailsData[0]);
+  const [nextVideo, setNextVideo] = useState(videoData);
+
+  useEffect(() => {
+    setCurrentVideo(videoDetailsData[0]);
+    setNextVideo(videoData);
+  }, []);
+
+  const handleClick = (videoId) => {
+    const selectedVideo = videoDetailsData.find(
+      (video) => video.id === videoId
+    );
+
+    setCurrentVideo(selectedVideo);
+
+    const updatedNextVideo = videoData.filter((video) => video.id !== videoId);
+    setNextVideo(updatedNextVideo);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <MainVideo video={currentVideo} handleClick={handleClick} />
+      <Comment comments={currentVideo.comments} />
+      <VideoDetails video={videoData} />
+      <NextVideo videos={nextVideo} handleClick={handleClick} />
     </div>
   );
 }
