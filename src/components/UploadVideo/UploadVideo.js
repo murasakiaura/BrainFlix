200415212e9979be2b0b311b97a8c9a1ef37a897
murasakiaura <isabella002211@gmail.com>
2024-08-12 00:33:54 +0100

    sprint3

--- src/components/UploadVideo/UploadVideo.js
+++ src/components/UploadVideo/UploadVideo.js
@@ -1,8 +1,25 @@
 import "./UploadVideo.scss";
 import publishbutton from "../../assets/Icons/publish.svg";
 import videoimage from "../../assets/images/Upload-video-preview.jpg";
+import axios from "axios";
+import { useState } from "react";
+
 
 function UploadVideo() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/videos', { title, description })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="UploadVideo">
       <div className="UploadVideo__title">
@@ -21,45 +38,46 @@ function UploadVideo() {
           </div>
 
           <div className="UploadVideo__formcard">
-            <div className="UploadVideo__formbody">
-              <legend className="UploadVideo__formtitle">
-                TITLE YOUR VIDEO
-              </legend>
-              <form>
+            <form onSubmit={handleSubmit}>
+              <div className="UploadVideo__formbody">
+                <legend className="UploadVideo__formtitle">
+                  TITLE YOUR VIDEO
+                </legend>
                 <input
                   className="UploadVideo__forminput"
                   type="text"
                   placeholder="Add a title to your video"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
                 />
-              </form>
 
-              <legend className="UploadVideo__formtitle">
-                ADD A VIDEO DESCRIPTION
-              </legend>
-              <form>
+                <legend className="UploadVideo__formtitle">
+                  ADD A VIDEO DESCRIPTION
+                </legend>
                 <input
                   className="UploadVideo__forminput UploadVideo__forminput--input"
                   type="text"
                   placeholder="Add a description to your video"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
                 />
-              </form>
-            </div>
+              </div>
+
+              <div className="UploadVideo__buttoncard">
+                <div className="UploadVideo__buttoncontainer">
+                  <img
+                    className="UploadVideo__buttonicon"
+                    src={publishbutton}
+                    alt="publishbutton"
+                  />
+                  <button className="UploadVideo__button" type="submit">
+                    PUBLISH
+                  </button>
+                </div>
+                <button type="button" className="UploadVideo__button2">CANCEL</button>
+              </div>
+            </form>
           </div>
-        </div>
-        <div className="UploadVideo__buttoncard">
-          <div className="UploadVideo__buttoncontainer">
-            <img
-              className="UploadVideo__buttonicon"
-              src={publishbutton}
-              alt="publishbutton"
-            />
-            <button className="UploadVideo__button" type="submit">
-              PUBLISH
-            </button>
-          </div>
-          <button className="UploadVideo__button2" type="submit">
-            CANCEL
-          </button>
         </div>
       </div>
     </div>
